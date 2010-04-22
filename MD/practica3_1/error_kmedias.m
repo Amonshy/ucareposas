01171c9d157f8ee9af894216289f00049c94943b
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-04-22 22:34:01 +0000

    Terminada mi parte de la practica 3_1, la base de datos de titanic y del banco.

--- MD/practica3_1/error_kmedias.m
+++ MD/practica3_1/error_kmedias.m
@@ -4,15 +4,34 @@ function [error,a] = error_kmedias(k, n, fichero_datos)
 % n: numero de semillas diferentes que se van a utilizar
 % fichero_datos: nombre del fichero de datos que se le pasa a la instruccion de weka
 
-datos = textread(fichero_datos,'%s','delimiter','\n','whitespace','');
-indice = find(ismember(datos, '@DATA')==1 | ismember(datos, '@data')==1);
-datos = datos(indice+1:end,1); % Matriz con los datos (desde '@data' al final)
-N = size(datos,1)
-a = round(N*rand(1,n));
-for j=1:length(a),
-	for i=1:k,
-		orden = sprintf('!java -classpath ";weka.jar" weka.clusterers.SimpleKMeans -N %d -S %d -t %s > salida.txt', i, a(j), fichero_datos);
-		eval(orden);
-		error(j,i) = obtener_error('salida.txt');
+[file, err] = fopen(fichero_datos,'r');
+if file ~= -1
+
+    %buscamos data para contar el numero de datos a partir de ahi
+    linea = fgets(file);
+    while lower(linea(1:5)) ~= '@data'
+        linea = fgets(file);
+    end;
+
+    %Una vez encontrado data, contamos el numero de datos
+    N = 0;
+    while ~feof(file)
+        N = N +1;   
+        linea = fgets(file);    
+    end;
+    
+    fclose(file);
+    
+	a = round(N*rand(1,n));
+	
+	for j=1:length(a),
+		for i=1:k,
+			orden = sprintf('!java -classpath ";weka.jar" weka.clusterers.SimpleKMeans -N %d -S %d -t %s > salida.txt', i, a(j), fichero_datos);
+			eval(orden);
+			error(j,i) = obtener_error('salida.txt');
+		end
 	end
+
+else
+    err    
 end
