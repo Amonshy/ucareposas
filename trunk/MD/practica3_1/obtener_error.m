ae90e9f0ebfb499194fe490307f6e46b4d3aa56a
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-04-22 22:34:01 +0000

    Terminada mi parte de la practica 3_1, la base de datos de titanic y del banco.

--- trunk/MD/practica3_1/obtener_error.m
+++ trunk/MD/practica3_1/obtener_error.m
@@ -1,13 +1,12 @@
 function error = obtener_error(fichero)
   mf = fopen(fichero,'rt');
+  error = 0;
   if mf ~= -1
-    frase = 'Within cluster sum of squared errors';
+    frase = 'Within cluster sum of squared errors: ';
     linea = fgets(mf);
-    while ~feof(mf)
-      if length(linea) > length(frase)
-	if linea (1:length(frase)) == false
-	  error = str2num(linea(length(frase)+1:end));
-	end
+    while ~feof(mf),
+      if length(linea) > length(frase) & linea(1:length(frase)) == frase,
+	              error = str2num(linea(length(frase)+1:end));
       end
       linea = fgets(mf);
     end
