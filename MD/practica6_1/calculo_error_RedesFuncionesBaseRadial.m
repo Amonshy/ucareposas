57446abb01667adbd3095487eb6fb0c05071773f
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-05-25 17:01:47 +0000

    Añadidos los metodos que faltaban

---
+++ MD/practica6_1/calculo_error_RedesFuncionesBaseRadial.m
@@ -0,0 +1,8 @@
+function error = calculo_error_RedesFuncionesBaseRadial(vecinos,fichero_train, fichero_test)
+% error = calculo_error_RedesFuncionesBaseRadial(numMax_vecino, fichero_train, fichero_test,rango)
+% Calcula el error para los K-vecinos, recibe el numero de vecinos y
+% los fichero de test y entrenamiento.
+    orden = sprintf('!java -classpath ";weka.jar" weka.classifiers.lazy.IBk -K %d -t %s -T %s> salida.txt', vecinos, fichero_train,fichero_test);
+    eval(orden);
+    error = obtener_error('salida.txt');
+end
