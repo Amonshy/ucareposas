a8a7a4a4b375f406ea96635ac0d6ef6e8b039616
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-05-25 17:50:21 +0000

    Añadido redes de funciones de base radial

--- trunk/MD/practica6_1/calculo_error_RedesFuncionesBaseRadial.m
+++ trunk/MD/practica6_1/calculo_error_RedesFuncionesBaseRadial.m
@@ -1,8 +1,8 @@
-function error = calculo_error_RedesFuncionesBaseRadial(vecinos,fichero_train, fichero_test)
-% error = calculo_error_RedesFuncionesBaseRadial(numMax_vecino, fichero_train, fichero_test,rango)
-% Calcula el error para los K-vecinos, recibe el numero de vecinos y
-% los fichero de test y entrenamiento.
-    orden = sprintf('!java -classpath ";weka.jar" weka.classifiers.lazy.IBk -K %d -t %s -T %s> salida.txt', vecinos, fichero_train,fichero_test);
+function error = calculo_error_RedesFuncionesBaseRadial(numFunciones, semilla,fichero_train, fichero_test)
+% error = calculo_error_RedesFuncionesBaseRadial(numFunciones,semilla,fichero_train, fichero_test)
+% Calcula el error para las redes de funciones de base radial, recibe el numero de funciones de base radial,
+% la semilla y los fichero de test y entrenamiento.
+    orden = sprintf('!java -classpath ";weka.jar" weka.classifiers.functions.RBFNetwork -B %i -S %i -R 1.0E-8 -M -1 -W 0.1 -t %s -T %s > salidaRBF.txt',numFunciones,semilla, fichero_train,fichero_test);
     eval(orden);
     error = obtener_error('salida.txt');
 end
--- trunk/MD/practica6_1/estimar_error_RedesFuncionesBaseRadial.m
+++ trunk/MD/practica6_1/estimar_error_RedesFuncionesBaseRadial.m
@@ -1,17 +1,19 @@
-function [ errorPromedio k_optima ] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
-% [ errorPromedio k_optima ] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
-%Estima el error medio del clasificador K-Vecinos, a partir de los 10
+function [errorPromedio numFuncionesOptima numSemilla] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
+% [errorPromedio numFuncionesOptima numSemilla] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
+%Estima el error medio de las redes funciones de base radial , a partir de los 10
 %pares de entrenamiento y test, generados en la carpeta 10kfoldoriginal.
 %nombre BD será el nombre de la base de datos bank o titanic.
 
 nombreEstandarFicheroTrain = ['10kfoldoriginal/' nombre_bd '/' nombre_bd '_kfcv_train'];
 nombreEstandarFicheroTest = ['10kfoldoriginal/'  nombre_bd '/' nombre_bd  '_kfcv_test'];
 
-%Numero máximo de vecinos que vamos a utilizar y que se ha obtenido
-%mediante una prueba previa y generacion de una gráfica.
-num_maximo_vecinos = 15;
+%Numero máximo de semillas y neuronas que vamos a utilizar.
+num_maximo_semillas = 1;
+num_maximo_funciones = 3;
+%Semillas generadas aleatoriamente
+semillas = round(100*rand(1,num_maximo_semillas));
 
-%Primero obtenemos el k optimo para cada uno de las 10 muestras del k-fold
+%Primero obtenemos la neurona y la semilla optima para cada una de las 10 muestras del k-fold
 %cross-validation que se han generado. Para ello utilizaremos de nuevo un
 %k-fold-cross-validation para obtener 10 muestras de cada una de las
 %muestras
@@ -25,30 +27,42 @@ for i=1:10,
         ficheroTrain = ['10kfoldsecundario/' nombre_bd '/train' num2str(i,'%0.2d') '/' nombre_bd '_kfcv_train' num2str(i,'%0.2d') '_kfcv_train' num2str(j,'%0.2d') '.arff' ];
         ficheroTest = ['10kfoldsecundario/' nombre_bd '/train' num2str(i,'%0.2d') '/' nombre_bd '_kfcv_train' num2str(i,'%0.2d') '_kfcv_test' num2str(j,'%0.2d') '.arff'];
 
-        %Generamos los errores obtenidos para cada uno de los k vecinos desde 2
-        %hasta num_maximo_vecinos, para cada pliegue j.
-        for k=2:num_maximo_vecinos,
-            errores(j,k-1) = calculo_error_KNN(k,ficheroTrain, ficheroTest);
+        %Generamos los errores obtenidos para cada uno de los pares
+        %semilla/neurona para un fragmento j determinado
+        for k=1:num_maximo_semillas,
+            for l=1:num_maximo_funciones,
+               errores(k,l,j) = calculo_error_RedesFuncionesBaseRadial(l,semillas(k),ficheroTrain, ficheroTest);
+            end;
         end;
-
-        %Una vez obtenidos los errores hacemos la media de los errores y nos
-        %quedamos con el minimo, porque el mínimo error, será el k_optimo
-        [valor indice_k_optima] = min(mean(errores));
-        
     end;
-    k_op(i) = indice_k_optima;
+     
+    %Calculamos la media de errores cometidos por cada par semilla/neurona
+    %obtenido en cada fragmento j
+    mediaPorSemillasFunciones = mean(errores,3);
+
+    %Obtenemos para cada neurona cual ha sido su valor mínimo entre todas
+    %las semillas
+    [filaMin semilla] = min(mediaPorSEmillasFunciones);
+    %Obtenemos ahora el minimo de todas las neuronas
+    [columnaMin funciones] = min(filaMin);
+    
+    %Guardamos la neurona
+    funciones_op(i) = funciones;
+    %Guardamos la semilla
+    semilla_op(i) = semillas(semilla(funciones));
 end;
 
 for i=1:10,
 
-     % Ahora probamos los k_optimos obtenidos para cada pliegue principal y
+     % Ahora probamos las neuronas y la semilla para cada pliegue principal y
      % calculamos el error promedio obtenido.
      ficheroTrain = [nombreEstandarFicheroTrain  num2str(i,'%0.2d') '.arff' ];
      ficheroTest = [nombreEstandarFicheroTrain  num2str(i,'%0.2d') '.arff' ];
-     erroresPrincipal(i) = calculo_error_KNN(k_op(i)+1,ficheroTrain,ficheroTest);
+     erroresPrincipal(i) = calculo_error_RedesFuncionesBaseRadial(funciones_op(i),semilla_op(i),ficheroTrain,ficheroTest);
       
 end;
 
 errorPromedio = mean(erroresPrincipal);
-k_optima = mode(k_op);
+numFuncionesOptima = mode(funciones_op);
+semilla_optima = mode(semilla_op);
 end
