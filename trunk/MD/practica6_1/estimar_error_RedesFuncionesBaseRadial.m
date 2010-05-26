279b3559f1c2803130ea0e94d2c5afe86085a96e
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-05-26 06:56:59 +0000

    Añadidos ficheros de datos para generar graficas de redes de funciones base radial.

--- trunk/MD/practica6_1/estimar_error_RedesFuncionesBaseRadial.m
+++ trunk/MD/practica6_1/estimar_error_RedesFuncionesBaseRadial.m
@@ -1,5 +1,5 @@
-function [errorPromedio numFuncionesOptima numSemilla] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
-% [errorPromedio numFuncionesOptima numSemilla] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
+function [errorPromedio funciones_optima semilla_optima] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
+% [errorPromedio funciones_optima semilla_optima] = estimar_error_RedesFuncionesBaseRadial (nombre_bd)
 %Estima el error medio de las redes funciones de base radial , a partir de los 10
 %pares de entrenamiento y test, generados en la carpeta 10kfoldoriginal.
 %nombre BD será el nombre de la base de datos bank o titanic.
@@ -8,16 +8,16 @@ nombreEstandarFicheroTrain = ['10kfoldoriginal/' nombre_bd '/' nombre_bd '_kfcv_
 nombreEstandarFicheroTest = ['10kfoldoriginal/'  nombre_bd '/' nombre_bd  '_kfcv_test'];
 
 %Numero máximo de semillas y neuronas que vamos a utilizar.
-num_maximo_semillas = 1;
-num_maximo_funciones = 3;
+num_maximo_semillas = 5;
+num_maximo_funciones = 10;
 %Semillas generadas aleatoriamente
 semillas = round(100*rand(1,num_maximo_semillas));
-
 %Primero obtenemos la neurona y la semilla optima para cada una de las 10 muestras del k-fold
 %cross-validation que se han generado. Para ello utilizaremos de nuevo un
 %k-fold-cross-validation para obtener 10 muestras de cada una de las
 %muestras
 for i=1:10,
+    disp(num2str(i));
     for j=1:10,
         %El formato de los ficheros generados con k-fold para cada una de
         %las 10 muestras están en la ruta
@@ -35,21 +35,33 @@ for i=1:10,
             end;
         end;
     end;
-     
+
     %Calculamos la media de errores cometidos por cada par semilla/neurona
     %obtenido en cada fragmento j
     mediaPorSemillasFunciones = mean(errores,3);
 
-    %Obtenemos para cada neurona cual ha sido su valor mínimo entre todas
-    %las semillas
-    [filaMin semilla] = min(mediaPorSEmillasFunciones);
-    %Obtenemos ahora el minimo de todas las neuronas
-    [columnaMin funciones] = min(filaMin);
     
-    %Guardamos la neurona
-    funciones_op(i) = funciones;
-    %Guardamos la semilla
-    semilla_op(i) = semillas(semilla(funciones));
+    if (num_maximo_semillas > 1)
+        
+        %Obtenemos para cada neurona cual ha sido su valor mínimo entre todas
+        %las semillas
+        [filaMin semilla] = min(mediaPorSemillasFunciones);
+        %Obtenemos ahora el minimo de todas las neuronas
+        [columnaMin funciones] = min(filaMin);
+
+        %Guardamos la neurona
+        funciones_op(i) = funciones;
+        %Guardamos la semilla
+        semilla_op(i) = semillas(semilla(funciones));
+    else
+        %Cuando solo tenemos una semilla sacamos la funcion optima
+        [filaMin funciones] = min(mediaPorSemillasFunciones);
+
+        %Guardamos la neurona
+        funciones_op(i) = funciones;
+        %Guardamos la semilla
+        semilla_op(i) = semillas(1);
+    end
 end;
 
 for i=1:10,
@@ -63,6 +75,6 @@ for i=1:10,
 end;
 
 errorPromedio = mean(erroresPrincipal);
-numFuncionesOptima = mode(funciones_op);
-semilla_optima = mode(semilla_op);
+funciones_optima = funciones_op;
+semilla_optima = semilla_op;
 end
