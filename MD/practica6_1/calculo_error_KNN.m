754208011c21f8d4f078cf61aa45cb735d035078
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-06-01 11:02:27 +0000

    Subo el script, que no lo tenia y lo he hecho de nuevo pero me aprece a mi que está mal hecho porque la gráfica no tiene mucho sentido XD.

--- MD/practica6_1/calculo_error_KNN.m
+++ MD/practica6_1/calculo_error_KNN.m
@@ -1,5 +1,5 @@
 function error = calculo_error_KNN(vecinos,fichero_train, fichero_test)
-% error = calculo_error_KNN(numMax_vecino, fichero_train, fichero_test,rango)
+% error = calculo_error_KNN(numMax_vecino, fichero_train, fichero_test)
 % Calcula el error para los K-vecinos, recibe el numero de vecinos y
 % los fichero de test y entrenamiento.
     orden = sprintf('!java -classpath ";weka.jar" weka.classifiers.lazy.IBk -K %d -t %s -T %s> salida.txt', vecinos, fichero_train,fichero_test);
