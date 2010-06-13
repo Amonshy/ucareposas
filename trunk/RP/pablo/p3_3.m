f33682bc1446fb1e0fdd887efe82fa0ce39f3c41
pablopl87 <pablopl87@30dec19e-261b-11df-b934-63b7860c0424>
2010-06-13 16:44:56 +0000

    Terminadas las prácticas de patrones.

--- trunk/RP/pablo/p3_3.m
+++ trunk/RP/pablo/p3_3.m
@@ -1,17 +1,44 @@
-load iris;
+clear all
+close all
+load iris
 
-pat = x(2:3,:);
+x = x(2:3,:);
+y = y+1;
 
-clase1 = pat(:,find(y==1));
-clase2 = pat(:,find(y==2));
-clase3 = pat(:,find(y==3));
+figure,plotpat(x,y);
+axis([2 5 1 7])
+axis equal
 
-m1 = meanpat(clase1);
-m2 = meanpat(clase2);
-m3 = meanpat(clase3);
+% Calcular parámetros de las gaussianas
+for i=1:3
+  indices = find(y==i);
+  m{i} = meanpat(x(:,indices));
+  C{i} = covpat(x(:,indices));
+end
 
-c1 = covpat(clase1);
-c2 = covpat(clase2);
-c3 = covpat(clase3);
+% Calcular funciones de densidad
+[xi,yi] = meshgrid(2:0.01:5,1:0.01:7);
+for i=1:3,
+  pdf{i} = mvnpdf([xi(:) yi(:)],m{i}',C{i});
+  pdf{i} = reshape(pdf{i},size(xi));
+end
 
-plotpat(x,y);
+figure,imagesc(2:0.01:5,1:0.01:7,pdf{1}+pdf{2}+pdf{3}), axis xy
+axis([2 5 1 7])
+
+S1 = (pdf{1}>pdf{2}) & (pdf{1}>pdf{3});
+S2 = (pdf{2}>pdf{1}) & (pdf{2}>pdf{3});
+S3 = (pdf{3}>pdf{1}) & (pdf{3}>pdf{2});
+
+figure,
+imagesc(2:0.01:5,1:0.01:7,S1+2*S2+3*S3);axis xy
+axis([2 5 1 7])
+
+% Calcular funciones de densidad
+for i=1:3,
+  pdfdata(i,:) = mvnpdf(x',m{i}',C{i})';
+end
+[basura,clase] = max(pdfdata);
+
+errores = find(clase~=y);
+figure(1),hold on;plotpat(x(:,errores),'.');hold off
---
+++ trunk/RP/pablo/p6_2.m
@@ -0,0 +1,90 @@
+rand('seed', 0);
+randn('seed', 0);
+
+m1 = [0; 0]; C1 = [1 0.8; 0.8 2];
+m2 = [3; 3]; C2 = [1 -0.9; -0.9 2];
+clase1 = randnorm(m1, C1, 1000);
+clase2 = randnorm(m2, C2, 1000);
+%Clase a la que pertenecen
+y = [ones(1, 1000) ones(1, 1000)*2];
+%barajamos
+[x, y] = shuffle([x1 x2], y);
+m = meanpat(x);
+
+% a) Realizar un clasificador de mínima distancia con los primeros 1600 
+% datos
+
+xtrn = x(:,1:1600);
+ytrn = y(:,1:1600);
+
+xtest = x(:,1601:end);
+ytest = y(:,1601:end);
+%Entrenamiento
+modelo = zeros(size(xtrn,1),2);
+for i=1:2,
+    modelo(:,i) = meanpat(xtrn(:,find(ytrn==i)));
+end;
+
+%Clasificación
+cov = covpat(xtrn);
+for i=1:2,
+    media = modelo(:,i);
+    dist(i,:)=d_mahal(xtest,media,cov);
+end;
+[dummy,y] = min(dist);
+error = sum(ytest ~= y)/size(y,2) * 100
+
+
+% % b) Convertir los datos al espacio PCA y realizar el clasificador de míni-
+% % ma distancia. ¿Qué procedimiento da mejores resultados de clasificación 
+% % en los 400 datos restantes? Es razonable el resultado obtenido.
+disp('PCA');
+xtrn1 = subpat(xtrn,meanpat(xtrn));
+W = pca(xtrn1);
+
+datos = W * xtrn;
+
+%Entrenamiento
+modelo = zeros(size(datos,1),2);
+for i=1:2,
+    modelo(:,i) = meanpat(datos(:,find(ytrn==i)));
+end;
+
+xtest1 = W*xtest;
+
+%Clasificación
+cov = covpat(datos);
+for i=1:2,
+    media = modelo(:,i);
+    dist(i,:)=d_mahal(xtest1,media,cov);
+end;
+[dummy,y] = min(dist);
+error = sum(ytest ~= y)/size(y,2) * 100
+
+
+% % c) ¿Puedes predecir qué ocurre si usas Fisher?
+disp('Fisher');
+W = fisher(xtrn,ytrn);
+
+datos = W * xtrn;
+
+%Entrenamiento
+modelo = zeros(size(datos,1),2);
+for i=1:2,
+    modelo(:,i) = meanpat(datos(:,find(ytrn==i)));
+end;
+
+xtest1 = W*xtest;
+
+%Clasificación
+cov = covpat(datos);
+for i=1:2,
+    media = modelo(:,i);
+    dist(i,:)=d_mahal(xtest1,media,cov);
+end;
+[dummy,y] = min(dist);
+error = sum(ytest ~= y)/size(y,2) * 100
+
+% e) ¿Cómo son las fronteras entre una clase y otra? ¿Puedes dibujarlas?
+% plotpat(x, y); hold on;
+% plotbon(m1, C1, m2, C2, 'b'); hold off
